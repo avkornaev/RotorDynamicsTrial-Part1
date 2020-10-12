a43786cb3d114ee7eab0ed5feca9b34935e6f9bd
avkornaev <70199936+avkornaev@users.noreply.github.com>
2020-10-12 11:38:26 +0300

    Add files via upload

--- pressureField.m
+++ pressureField.m
@@ -6,29 +6,38 @@ r=static.r;
 L=static.L;
 m=static.m;
 s=static.s;
-x1=static.x1(2:end-1);%x1 excluding x1=0 and x1=2*pi,as well as p(0) and p(2*pi) are the same known value
-x3=static.x3;
-h=static.gap.h(2:end-1);
-hpow3=static.gap.hpow3(2:end-1);
-dhpow3dx1=static.gap.dhpow3dx1(2:end-1);
-u1=static.vel.u1(2:end-1);
-u2=static.vel.u2(2:end-1);
-du1hdx1=static.vel.du1hdx1(2:end-1);
+x1=static.x1(1:end-1);
+x3=static.x3(2:end-1);
+h=static.gap.h(1:end-1);
+hpow3=static.gap.hpow3(1:end-1);
+dhpow3dx1=static.gap.dhpow3dx1(1:end-1);
+u1=static.vel.u1(1:end-1);
+u2=static.vel.u2(1:end-1);
+du1hdx1=static.vel.du1hdx1(1:end-1);
 mu=static.mu;
 p0=static.p0;
 
+%From 1D to 2D fast transform (f(x1) -> f(x1,x3))
+h=meshgrid(h,x3);
+hpow3=meshgrid(hpow3,x3);
+dhpow3dx1=meshgrid(dhpow3dx1,x3);
+du1hdx1=meshgrid(du1hdx1,x3);
+u2=meshgrid(u2,x3);
+%surf(h)
+
 % Zero-value matriсes
-A=zeros(1,m(1));
-B=zeros(1,m(1));
-C=zeros(1,m(1));
-D=zeros(1,m(1));
-E=zeros(1,m(1));
-G=zeros(1,m(1));
+n=m(1)*m(3);%number of unknowns p(i,j)
+A=zeros(m(3),m(1));
+B=zeros(m(3),m(1));
+C=zeros(m(3),m(1));
+D=zeros(m(3),m(1));
+E=zeros(m(3),m(1));
+G=zeros(m(3),m(1));
 
-Lambda=zeros(m(1),m(1));
-Gamma=zeros(m(1),1);
+Lambda=zeros(n,n);
+Gamma=zeros(n,1);
 
-p=zeros(1,m(1));
+p=zeros(m(3),m(1));
 F1=0;F2=0;M=0;
 subF1=zeros(m(3),m(1));
 subF2=zeros(m(3),m(1));
@@ -39,19 +48,35 @@ subM=zeros(m(3),m(1));
 
 % Matrices of coefficients 
 %A=;
-B=B;
-C=C;
+%B=;
+%C=;
 %D=;
 %E=;
 %G=;
 %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 % Matrix form for the Reynolds equation: Lambda*P=Gamma
-for j=1:m(1)
-    Lambda(j,j) =A(1,j);%filling the diagonal components of the 'A' matrix
+for k=1:n %number of the Reynolds equation in a specific point (i,j)
+    i=1+floor((k-1)/m(1));%column #
+    j=k-(i-1)*m(1);%row #
+    %[i;j;k]
+    Lambda(k,k) =A(i,j);%filling the diagonal components of the 'A' matrix
+    Gamma(k)=G(i,j);%filling the right part of the matrix Reynolds equation
+    if i~=1
+        Lambda(k,k-m(1))=C(i,j);
+    else
+        Gamma(k)=Gamma(k)-C(i,j)*p0(1);
+    end
     %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    %Enter your code here and fill the 'Lambda' and the 'Gamma' matrices 
+    %Enter your code here and complete filling the 'Lambda' and the 'Gamma'
 
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -59,29 +84,30 @@ for j=1:m(1)
     
     %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 end
-%1D pressure field
-p=Lambda\Gamma%solving matrix equation
-p=[p0 p' p0];%adding the extra point with the known pressure value
-%2D pressure field
-p=meshgrid(p,x3);
 
+%2D pressure field
+p=Lambda\Gamma;%solving matrix equation
+
+p=reshape(p,[m(1) m(3)]);%a row to matrix
+p=p';%matrix transpose 
+p=[ p p(:,1)];%a row for the 2*pi angle
 
 %Projections of the resulting force and the torque
 x1=static.x1;%return the full matrix x1 
+x3=static.x3;%return the full matrix x1 
 h=static.gap.h;%return the full matrix h
 u1=static.vel.u1;%return the full matrix u1
+p=[p0(1)*ones(1,length(x1)); p ;...
+   p0(2)*ones(1,length(x1))];%adding the extra rows with the known pressure values
 for i=1:length(x3)
     for j=1:length(x1)
         subF1(i,j)=-p(i,j)*sin(x1(j)/r);
-        %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        %Enter your code here
-        %subF2(i,j)=;
-        %subM(i,j)=;
-        %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        subF2(i,j)=-p(i,j)*cos(x1(j)/r);
+        subM(i,j)=-r*(diffmy(j,p(i,:),s(1))*(h(j)/2)+u1(j)*mu/h(j));
     end
 end
-F1=dblintegral(subF1)*(2*pi*r*L);%the correct answer is about 0 N    
-F2=dblintegral(subF2)*(2*pi*r*L);%the correct answer is about 10.5 N    
-M = dblintegral(subM)*(2*pi*r*L); %the correct answer is about 0.0034 N*m    
+F1=dblintegral(subF1)*(2*pi*r*L);% N    
+F2=dblintegral(subF2)*(2*pi*r*L);% N    
+M = dblintegral(subM)*(2*pi*r*L);% N*m
 end
 
--- velFunc.m
+++ velFunc.m
@@ -11,21 +11,19 @@ h=kinem.gap.h;
 V1=kinem.V1;
 V2=kinem.V2;
 
-u1=zeros(1,length(x1));
-u2=zeros(1,length(x1));
-du1hdx1=zeros(1,length(x1));
+du1hdx1=zeros(1,m(1));
 
 %Calculation
 omega=2*pi*n_rps;
 
 %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 %Enter your code here
-%u1=;% tangential component
-%u2=;% normal component
+u1=omega*r+V1*cos(x1/r)-V2*sin(x1/r);% tangential component
+u2=        V1*sin(x1/r)+V2*cos(x1/r);% normal component
 u1h=u1.*h;
 
 for j=1:length(x1)
-    %du1hdx1(j)=;% derivative d(u1*h)/dx1;
+    du1hdx1(j)=diffmy(j,u1h,s(1));% derivative d(u1*h)/dx1;
 end
 %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
